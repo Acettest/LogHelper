a8aa623cc087351186c22d0d3a78795e2aca6cea
msheng <shengmiao2016@163.com>
2018-10-07 14:57:36 +0800

    log project that can be used in any other project in the same solution

---
+++ LogHelper/Log.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+namespace LogHelper
+{
+    static public class Log
+    {
+        static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Debug(string message)
+        {
+            log.Debug(message);
+        }
+
+        public static void Error(string message)
+        {
+            log.Error(message);
+        }
+
+        public static void Info(string message)
+        {
+            log.Info(message);
+        }
+
+        public static void Fatal(string message)
+        {
+            log.Fatal(message);
+        }
+
+        public static void Warn(string message)
+        {
+            log.Warn(message);
+        }
+
+    }
+}
